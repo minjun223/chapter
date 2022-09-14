d3238a5f46be8f84cba7dc34aa402f2fa98e341f
김민준 <80810346+minjun223@users.noreply.github.com>
2022-09-14 12:29:08 +0900

    practice

---
+++ app/src/main/java/com/example/app_part2_chap07/CountUpView.kt
@@ -0,0 +1,46 @@
+package com.example.app_part2_chap07
+
+import android.content.Context
+import android.os.SystemClock
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class CountUpView(context: Context,
+    attrs: AttributeSet? = null): AppCompatTextView(context, attrs) {
+
+       private var startTimeStamp: Long= 0L
+
+        private val countUpAction: Runnable = object: Runnable{
+            override fun run() {
+                val currentTimeStamp = SystemClock.elapsedRealtime()
+
+                val countTimeSeconds = ((currentTimeStamp - startTimeStamp)/1000L).toInt()
+
+                updateCountTime(countTimeSeconds)
+
+                handler?.postDelayed(this, 1000L)
+
+
+            }
+        }
+
+
+    fun startCountUp(){
+        startTimeStamp = SystemClock.elapsedRealtime()
+        handler?.post(countUpAction)
+    }
+
+    fun clearCountTime(){
+        updateCountTime(0)
+    }
+
+    fun stopCountUp(){
+        handler?.removeCallbacks(countUpAction)
+    }
+    private fun updateCountTime(countTimerSeconds: Int){
+        val minutes = countTimerSeconds / 60
+        val seconds = countTimerSeconds % 60
+
+        text = "%02d:%02d".format(minutes, seconds)
+    }
+}
